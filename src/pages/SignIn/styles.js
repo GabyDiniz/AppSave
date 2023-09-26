a3b7dde4b6ca97762acd90bbb5291242001a1eaa
Gabriely Diniz <gabrielydiniz723@gmail.com>
2023-09-26 11:26:34 -0300

    26-09

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components"
+
+export const Background = styled.View`
+    flex: 1;
+    background-color: #F0F4FF;
+`;
+
+export const Container = styled.KayboardAvoidingView``;
+
+export const Logo = styled.image``;
+
+export const AreaInput = styled.View``;
+
+export const Input = styled.TextInput``;
